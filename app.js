06d564b589af77c9852bd595bab4b54a39772859
AmberYounus <65040261+AmberYounus@users.noreply.github.com>
2023-05-12 01:57:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+//to make the element draggable
+dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    //cursor move
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+//styling through dom
+document.getElementById("mydiv").style.backgroundColor ="#f1f1f1";
+document.getElementById("mydiv").style.textAlign ="center";
+document.getElementById("mydiv").style.position="absolute";
+document.getElementById("mydiv").style.zIndex ="9";
+document.getElementById("mydiv").style.border ="1px solid #d3d3d3";
+// document.getElementById("mydiv").style.top ="155px";
+// document.getElementById("mydiv").style.right ="660px";
+
+
+
